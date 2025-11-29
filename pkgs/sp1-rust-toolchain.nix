d925b694bd9c4386bca0170247d8c2ffc6397cbe
Dmytro Onypko <vaporif@proton.me>
2025-11-29 03:29:26 +0000

    update sp1

--- pkgs/sp1-rust-toolchain.nix
+++ pkgs/sp1-rust-toolchain.nix
@@ -9,20 +9,40 @@
   gcc-unwrapped,
 }:
 
+let
+  platform = {
+    x86_64-linux = {
+      name = "x86_64-unknown-linux-gnu";
+      sha256 = "sha256-n+L+4BkI/M8y6hg3N5zoNqJPPaw6gMok4zyHplmcNuw=";
+    };
+    aarch64-linux = {
+      name = "aarch64-unknown-linux-gnu";
+      sha256 = "sha256-pvsdzN9VUrMd1QJM8RqrIM8PyJRW4JxD4+CsaQ0NQTQ=";
+    };
+    x86_64-darwin = {
+      name = "x86_64-apple-darwin";
+      sha256 = "sha256-jsQ18AWBlIAkyBMpIesUSdQLLJ6k5imZ/AGW2F0ek5A=";
+    };
+    aarch64-darwin = {
+      name = "aarch64-apple-darwin";
+      sha256 = "sha256-GvU/SINTrSSi+bHXCvNot0IjHDkG8EiMbbn9xZWfhpo=";
+    };
+  }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+in
 stdenv.mkDerivation rec {
   pname = "sp1-rust-toolchain";
-  version = "1.81.0";
+  version = "1.91.1";
 
   src = fetchurl {
-    url = "https://github.com/succinctlabs/rust/releases/download/v${version}/rust-toolchain-x86_64-unknown-linux-gnu.tar.gz";
-    sha256 = "sha256-+Ll4tE/e39nz3+oSfuEfE+4v2q/96iYZSJ3/e755HM4=";
+    url = "https://github.com/succinctlabs/rust/releases/download/succinct-${version}/rust-toolchain-${platform.name}.tar.gz";
+    inherit (platform) sha256;
   };
 
-  nativeBuildInputs = [
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [
     autoPatchelfHook
   ];
 
-  buildInputs = [
+  buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
     xz
     zlib
     ncurses
@@ -46,6 +66,6 @@ stdenv.mkDerivation rec {
     description = "Succinct Labs Rust toolchain";
     homepage = "https://github.com/succinctlabs/rust";
     license = licenses.mit;
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
   };
 }
